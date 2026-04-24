e56157ecf07b26427bc6d82da7cc6d7558f9785f
Matthew Ward <matthewsward28@gmail.com>
2026-04-24 15:05:15 -0700

    moved stuff around

--- script/stage_1_script/script_mlp.py
+++ script/stage_1_script/script_mlp.py
@@ -1,32 +1,33 @@
 from local_code.stage_1_code.Dataset_Loader import Dataset_Loader
-from local_code.stage_1_code.Method_SVM import Method_SVM
+from local_code.stage_1_code.Method_MLP import Method_MLP
 from local_code.stage_1_code.Result_Saver import Result_Saver
 from local_code.stage_1_code.Setting_KFold_CV import Setting_KFold_CV
 from local_code.stage_1_code.Setting_Train_Test_Split import Setting_Train_Test_Split
 from local_code.stage_1_code.Evaluate_Accuracy import Evaluate_Accuracy
 import numpy as np
+import torch
 
-#---- Support Vector Machine script ----
+#---- Multi-Layer Perceptron script ----
 if 1:
     #---- parameter section -------------------------------
-    c = 1.0
     np.random.seed(2)
+    torch.manual_seed(2)
     #------------------------------------------------------
 
     # ---- objection initialization setction ---------------
     data_obj = Dataset_Loader('toy', '')
-    data_obj.dataset_source_folder_path = '../../data/stage_1_data/'
+    data_obj.dataset_source_folder_path = './data/stage_1_data/'
     data_obj.dataset_source_file_name = 'toy_data_file.txt'
 
-    method_obj = Method_SVM('support vector machine', '')
-    method_obj.c = c
+    method_obj = Method_MLP('multi-layer perceptron', '')
 
     result_obj = Result_Saver('saver', '')
-    result_obj.result_destination_folder_path = '../../result/stage_1_result/SVM_'
+    result_obj.result_destination_folder_path = './result/stage_1_result/MLP_'
     result_obj.result_destination_file_name = 'prediction_result'
 
     setting_obj = Setting_KFold_CV('k fold cross validation', '')
-    #setting_obj = Setting_Train_Test_Split('train test split', '')
+    #setting_obj = Setting_Tra
+    # in_Test_Split('train test split', '')
 
     evaluate_obj = Evaluate_Accuracy('accuracy', '')
     # ------------------------------------------------------
@@ -37,7 +38,7 @@ if 1:
     setting_obj.print_setup_summary()
     mean_score, std_score = setting_obj.load_run_save_evaluate()
     print('************ Overall Performance ************')
-    print('SVM Accuracy: ' + str(mean_score) + ' +/- ' + str(std_score))
+    print('MLP Accuracy: ' + str(mean_score) + ' +/- ' + str(std_score))
     print('************ Finish ************')
     # ------------------------------------------------------
     
