b31cd597ea44e1ec1a3d0b4f760afabab328f659
Matthew Ward <matthewsward28@gmail.com>
2026-04-24 16:48:27 -0700

    result saver

--- local_code/stage_2_code/Result_Saver.py
+++ local_code/stage_2_code/Result_Saver.py
@@ -8,15 +8,16 @@ Concrete ResultModule class for a specific experiment ResultModule output
 from local_code.base_class.result import result
 import pickle
 
-
 class Result_Saver(result):
     data = None
-    fold_count = None
     result_destination_folder_path = None
     result_destination_file_name = None
     
     def save(self):
         print('saving results...')
-        f = open(self.result_destination_folder_path + self.result_destination_file_name + '_' + str(self.fold_count), 'wb')
+        full_path = self.result_destination_folder_path + self.result_destination_file_name
+        
+        f = open(full_path, 'wb')
         pickle.dump(self.data, f)
         f.close()
+        print(f'Results saved to {full_path}')
